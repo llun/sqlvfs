e556eb996a07823050a5c43c3974c084c87662a6
Maythee Anegboonlap <null@llun.me>
2021-12-12 11:31:21 +0800

    Init

---
+++ feeds/index.ts
@@ -0,0 +1,97 @@
+import crypto from 'crypto'
+import fs from 'fs'
+import path from 'path'
+import fetch from 'node-fetch'
+import { getInput, setFailed } from '@actions/core'
+import { parseXML, parseAtom, parseRss, Site } from './parsers'
+
+export async function loadFeed(
+  title: string,
+  url: string
+): Promise<Site | null> {
+  try {
+    const data = await fetch(url, {
+      headers: {
+        'User-Agent': 'llun/feeds'
+      }
+    }).then((response) => response.text())
+    const xml = await parseXML(data)
+    if (xml.rss) return parseRss(title, xml)
+    if (xml.feed) return parseAtom(title, xml)
+    return null
+  } catch (error) {
+    console.error(error.message)
+    console.error(error.stack)
+    return null
+  }
+}
+
+export async function readOpml(opmlContent: string): Promise<any> {
+  const input = await parseXML(opmlContent)
+  const body = input.opml.body
+  const outlines = body[0].outline
+  return outlines.reduce((out, outline) => {
+    const category = outline.$.title
+    const items = outline.outline
+    out.push({
+      category,
+      items: items && items.map((item) => item.$)
+    })
+    return out
+  }, [])
+}
+exports.readOpml = readOpml
+
+export function createCategoryDirectory(
+  rootDirectory: string,
+  category: string
+) {
+  try {
+    const stats = fs.statSync(path.join(rootDirectory, category))
+    if (!stats.isDirectory()) {
+      throw new Error(
+        `${path.join(rootDirectory, category)} is not a directory`
+      )
+    }
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      throw new Error(`Fail to access ${rootDirectory}`)
+    }
+    fs.mkdirSync(path.join(rootDirectory, category), { recursive: true })
+  }
+}
+
+export async function writeFeedsContent() {
+  try {
+    const contentDirectory = getInput('outputDirectory', {
+      required: true
+    })
+    const feedsFile = getInput('opmlFile', { required: true })
+    const opmlContent = fs.readFileSync(feedsFile).toString('utf8')
+    const opml = await readOpml(opmlContent)
+    for (const category of opml) {
+      const { category: title, items } = category
+      createCategoryDirectory(contentDirectory, title)
+      if (!items) continue
+      console.log(`Load category ${title}`)
+      for (const item of items) {
+        const feedData = await loadFeed(item.title, item.xmlUrl)
+        if (!feedData) {
+          continue
+        }
+        console.log(`Load ${feedData.title}`)
+        const sha256 = crypto.createHash('sha256')
+        sha256.update(feedData.title)
+        const hexTitle = sha256.digest('hex')
+        fs.writeFileSync(
+          path.join(contentDirectory, title, `${hexTitle}.json`),
+          JSON.stringify(feedData)
+        )
+      }
+    }
+  } catch (error) {
+    console.error(error.message)
+    console.error(error.stack)
+    setFailed(error)
+  }
+}
---
+++ index.ts
@@ -0,0 +1,94 @@
+import { Database } from 'sqlite3'
+import fs from 'fs'
+import path from 'path'
+import crypto from 'crypto'
+import { writeFeedsContent } from './feeds'
+import { Site } from './feeds/parsers'
+
+const CONTENT_PATH = './contents'
+
+function getCategories() {
+  return fs.readdirSync(CONTENT_PATH)
+}
+
+function getSites(category: string): Site[] {
+  const categoryPath = path.join(CONTENT_PATH, category)
+  const sites = fs.readdirSync(categoryPath)
+  return sites.map((site) =>
+    JSON.parse(fs.readFileSync(path.join(categoryPath, site)).toString('utf-8'))
+  )
+}
+
+function hash(input: string) {
+  return crypto.createHash('sha256').update(input).digest('hex')
+}
+
+async function main() {
+  if (process.env.LOAD_FEEDS) {
+    await writeFeedsContent()
+  }
+  fs.rmSync('./data.sqlite3')
+  const database = new Database('./data.sqlite3')
+  database.serialize(async () => {
+    database.run('create table Categories (name TEXT PRIMARY KEY)')
+    database.run(
+      'create table Sites (key TEXT PRIMARY KEY, title TEXT NOT NULL UNIQUE, category TEXT NOT NULL, url TEXT, description TEXT, updated_at INTEGER NOT NULL)'
+    )
+    database.run(
+      'create table Entries (key TEXT PRIMARY KEY, site TEXT NOT NULL, category TEXT NOT NULL, title TEXT NOT NULL, url TEXT NOT NULL, content TEXT NOT NULL, content_date INTEGER, updated_at INTEGER NOT NULL)'
+    )
+
+    const categories = getCategories()
+    const insertCategoryStmt = database.prepare(
+      'insert into Categories values (?)'
+    )
+    const insertSiteStmt = database.prepare(
+      'insert into Sites values (?, ?, ?, ?, ?, ?)'
+    )
+    const insertEntryStmt = database.prepare(
+      'insert into Entries values (?, ?, ?, ?, ?, ?, ?, ?)'
+    )
+    for (let category of categories) {
+      insertCategoryStmt.run(category)
+
+      const sites = getSites(category)
+      for (let site of sites) {
+        const siteKey = hash(site.title)
+        const updatedAt = site.updatedAt || Date.now()
+        insertSiteStmt.run(
+          siteKey,
+          site.title,
+          category,
+          site.link,
+          site.description,
+          Math.floor(updatedAt / 1000)
+        )
+
+        for (let entry of site.entries) {
+          const entryKey = hash(`${entry.title}${entry.link}`)
+          const now = Date.now()
+          const contentDate = entry.date
+            ? Math.floor(entry.date / 1000)
+            : Math.floor(now / 1000)
+          const updatedAt = Math.floor((entry.date || now) / 1000)
+          insertEntryStmt.run(
+            entryKey,
+            siteKey,
+            category,
+            entry.title,
+            entry.link,
+            entry.content,
+            contentDate,
+            updatedAt
+          )
+        }
+      }
+    }
+
+    database.run('pragma journal_mode = delete;')
+    database.run('pragma page_size = 1024;')
+    database.run('vacuum')
+  })
+}
+
+main()
